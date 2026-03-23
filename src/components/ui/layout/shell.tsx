75c610b63f7c30bba7e87b8c909500df7c676f43
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2026-03-22 23:25:27 -0400

    Simplifying css class

--- src/components/ui/layout/shell.tsx
+++ src/components/ui/layout/shell.tsx
@@ -1,7 +1,7 @@
 export default function Shell({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4">
-      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/60 md:min-h-min">
+      <div className="min-h-screen flex-1 rounded-xl bg-muted/60 md:min-h-min">
         {children}
       </div>
     </div>
