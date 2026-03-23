0d74ab0a03544517d5ba560eb79d66406be68b98
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2026-03-22 22:57:07 -0400

    Adding image to header

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -1,5 +1,9 @@
+import heroImg from "@/assets/vite.svg";
+
 export default function Header() {
   return (
-    <header className="flex h-12 shrink-0 items-center gap-2 border-b px-4 border-slate-300"></header>
+    <header className="flex h-12 shrink-0 items-center gap-2 border-b px-4 border-slate-300">
+      <img src={heroImg} alt="Sharp Skies Logo" className="h-8 w-8" />
+    </header>
   );
 }
