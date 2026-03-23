558c0a54f06e29e9f8f1f7cee07a08f67144e7be
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2026-03-22 23:20:46 -0400

    Editor page placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import Header from "./components/ui/header/header";
+import EditorPage from "./pages/editor-page/editor-page";
 
 function App() {
   return (
     <>
       <Header />
-      <div>Hello world</div>
+      <div className="flex flex-1 flex-col gap-4 p-4">
+        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/60 md:min-h-min">
+          <EditorPage />
+        </div>
+      </div>
     </>
   );
 }
