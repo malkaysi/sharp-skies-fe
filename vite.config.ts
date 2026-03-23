883eee114352990267b22a10313735cb7672e35b
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2026-03-22 22:45:42 -0400

    Fixing alias issues

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,18 @@ import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    ],
+  },
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
