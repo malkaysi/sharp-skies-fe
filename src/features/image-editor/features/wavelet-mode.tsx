7e43021c0d9d6cc6ad2658e8322b2f26dd1162d1
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2026-05-25 20:57:23 -0400

    commenting out the stepper

--- src/features/image-editor/features/wavelet-mode.tsx
+++ src/features/image-editor/features/wavelet-mode.tsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
 import InfoBox from "../components/wavelet-mode/info-box";
-import LayerCountStepper from "../components/wavelet-mode/layer-count-stepper";
 
 export default function WaveletMode() {
-  const [layerCount, setLayerCount] = useState(6);
-
   return (
     <div>
       <InfoBox />
-      <LayerCountStepper count={layerCount} onChange={setLayerCount} />
+      {/* <LayerCountStepper count={layerCount} onChange={setLayerCount} /> */}
     </div>
   );
 }
