117fd62c93fc1560193a7143ceff1253601a9f4d
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2026-08-10 21:45:21 -0400

    Fixing vertical sizing

--- src/components/ui/layout/shell.tsx
+++ src/components/ui/layout/shell.tsx
@@ -1,7 +1,7 @@
 export default function Shell({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-1 flex-col gap-4">
-      <div className="h-full flex-1 rounded-xl bg-background min-h-0">
+      <div className="flex h-full flex-1 flex-col rounded-xl bg-background min-h-0">
         {children}
       </div>
     </div>
